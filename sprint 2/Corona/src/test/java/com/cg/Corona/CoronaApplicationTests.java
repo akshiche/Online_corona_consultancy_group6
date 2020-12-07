8e9e7d5eaed0f655713079b1181d51e0f5df39f0
akshiche <70131706+akshiche@users.noreply.github.com>
2020-12-07 15:06:44 +0530

    sprint_2_code

---
+++ sprint 2/Corona/src/test/java/com/cg/Corona/CoronaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.Corona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
